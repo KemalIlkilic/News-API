2d837996dd19f1c1ff2f88c16ff3123db11a5c54
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2024-08-10 20:08:58 +0300

    feat: Add filtering functionality to getAllProducts endpoint

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,18 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
