80b782d7ad8edf2b9b39bdf5b95608ed36eafb05
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2024-08-11 03:51:44 +0300

    chore: Refactor getAllProducts to support advanced filtering and sorting

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -14,9 +14,33 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
-  console.log(queryObject);
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  // Lazy Evaulation, "don't do the work until you absolutely have to."
+  /*
+  When you call Product.find(queryObject), instead of immediately going to the database and fetching results,
+  Mongoose (the ODM for MongoDB in Node.js) returns a Query object.
+  This object represents the query you want to perform, but it hasn't been executed yet.
+  Think of it like writing down instructions for a task, but not actually doing the task yet.
+  The Query object holds these "instructions" for the database query.
+  */
+  let query = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    query = query.sort(sortList);
+  } else {
+    query = query.sort("createdAt");
+  }
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    query = query.select(fieldsList);
+  }
+  const products = await query;
 
-  const products = await Product.find(queryObject);
+  //const products = await Product.find(queryObject)
+  //.sort(sort ? sort.split(",").join(" ") : "createdAt");
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
